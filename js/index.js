0eee41e646b7057576371bb04616104295b13598
jay-68 <kelvinngari1995@gmail.com>
2019-08-18 12:30:14 +0300

    Remove the about.html and creates cards

--- js/index.js
+++ js/index.js
@@ -9,7 +9,51 @@ function submitButton() {
     alert('month error');
   } else if (year == '' || year > 2019) {
     alert('year error');
-  }else{
+  } else {
     alert('check your data');
   }
+let maleNames=[]
+
+  var dD = Number(prompt("Enter date:"));
+  var mM = Number(prompt("Enter month:"));
+  var cC = Number(prompt("Enter century:"));
+  var yY = Number(prompt("Enter year:"));
+  function dayOfTheWeek() {
+    var d = Math.round((((cC / 4) - 2 * cC - 1) + ((5 * yY / 4)) + ((26 * (mM + 1) / 10)) + dD) % 7);
+    var pickedDay;
+    switch (d) {
+      case 0:
+        pickedDay = "Kwasi";
+        alert("Kwasi");
+        break;
+      case 1:
+        pickedDay = "Kwadwo";
+        alert("Kwadwo");
+        break;
+      case 2:
+        pickedDay = "Kwabena";
+        alert("Kwabena");
+        break;
+      case 3:
+        pickedDay = "Kwaku";
+        alert("Kwaku");
+        break;
+      case 4:
+        pickedDay = "Yaw";
+        alert("Yaw");
+        break;
+      case 5:
+        pickedDay = "Kofi";
+        alert("Kofi");
+        break;
+      case 6:
+        pickedDay = "Kwame";
+        alert("Kwame");
+        break;
+      default:
+        pickedDay = "You are lost!"
+    }
+  }
+  alert(dayOfTheWeek());
+  //var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];*/dddddddddddddddddddddddd
 }
